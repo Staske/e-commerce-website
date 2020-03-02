9e14c9f5b7aaa8b6e3a27dae67bf231953b1c55c
Valdas <staskevicius.valdas@gmail.com>
2020-03-02 22:42:03 +0200

    Adding switch and route components to project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 
-import Homepage from "./pages/homepage/homepage.component";
+import HomePage from "./pages/homepage/homepage.component";
+
+const ShoesPage = () => (
+  <div>
+    <h1>SHOES PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shoes" component={ShoesPage} />
+      </Switch>
     </div>
   );
 }
